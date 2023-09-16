3917f1ef6a81c85ecedae5c983719f53451fd151
Joél Collins <joelsewhere@gmail.com>
2023-09-16 02:08:12 -0500

    fixing typo

--- project_template/bin/build.sh
+++ project_template/bin/build.sh
@@ -2,7 +2,7 @@ echo -e "\n\n\x1B[1;42mMaking Django Database Migrations\x1B[0m\n\n"
 python django_site/manage.py makemigrations quarto
 python django_site/manage.py migrate
 python django_site/manage.py makemigrations
-python djnago_site/manage.py migrate
+python django_site/manage.py migrate
 
 SUPERUSER=false
 case "$1" in
